399941fceab82119c38b5ec317279bf302144b11
Shota Imaki <shota.imaki@icloud.com>
2020-12-25 20:54:58 +0900

    status code

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!bin/sh -eu
 
-find `dirname $0` -name "*.ipynb" -not -path "*/.ipynb_checkpoints/*" -exec papermill {} {} \;
-return $?
+find `dirname $0` -name "*.ipynb" -not -path "*/.ipynb_checkpoints/*" -exec sh -c 'for n; do papermill --cwd `dirname $n` $n $n || exit 1; done' sh {} +
+
+exit $?
